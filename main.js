f0a86e7a029f8f6c7ef7ff8624627a960e7f6253
jayasankar-shyam <jayasankar06shyam@gmail.com>
2021-06-19 12:16:46 +0530

    added skills

--- main.js
+++ main.js
@@ -15,3 +15,15 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+let animate = new Typed("#anim", {
+    strings: ["B.Tech Student", "Front End Developer"],
+    typeSpeed: 100,
+    backSpeed: 25,
+    loop: true,
+  });
+  let animat = new Typed("#anim2", {
+    strings: ["B.Tech Student", "Front End Developer"],
+    typeSpeed: 100,
+    backSpeed: 25,
+    loop: true,
+  });
